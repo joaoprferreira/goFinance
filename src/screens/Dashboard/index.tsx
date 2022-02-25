9f32d2c9265ce0a8d2d64fe1b654964e7126dec7
João Pedro <joaof082@gmail.com>
2022-02-25 10:59:20 -0300

    feat: Adicionando Header na tela
    Signed-off-by: João Pedro <joaof082@gmail.com>

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,17 @@
 import React from "react";
-import { Container, Title } from './styles'
+import {Text} from 'react-native'
+import {
+   Container, 
+   Header
+  } from './styles'
 
 export function Dashboard() {
 
   return (
     <Container>
-      <Title> Dashboard </Title>
+      <Header>
+        <Text>Elemento </Text>
+      </Header>
     </Container>
   )
 }
