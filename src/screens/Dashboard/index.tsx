ffeb5f4bddd71a5fbdde6f3690e778f9ef9d6344
João Pedro <joaof082@gmail.com>
2022-02-25 16:24:13 -0300

    feat: Iniciando os componentes da header
    Signed-off-by: João Pedro <joaof082@gmail.com>

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,16 +1,32 @@
 import React from "react";
-import {Text} from 'react-native'
+import { Text } from 'react-native'
 import {
-   Container, 
-   Header
-  } from './styles'
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UseGreeting,
+  UserName,
+} from './styles'
 
 export function Dashboard() {
 
   return (
     <Container>
       <Header>
-        <Text>Elemento </Text>
+        <UserWrapper>
+          <UserInfo>
+            <Photo source={
+              { uri: 'https://avatars.githubusercontent.com/u/49590645?v=4' }
+            } />
+            <User>
+              <UseGreeting>Olá</UseGreeting>
+              <UserName>João</UserName>
+            </User>
+          </UserInfo>
+        </UserWrapper>
       </Header>
     </Container>
   )
