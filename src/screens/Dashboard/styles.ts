8e5c376cb250d81d7481ae21dc315a68450e1710
João Pedro <joaof082@gmail.com>
2022-02-25 11:00:17 -0300

    feat: colocando cor de fundo e altura da header
    Signed-off-by: João Pedro <joaof082@gmail.com>

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -2,13 +2,11 @@
 
  export const Container =  styled.View`
   flex: 1;
-  justify-content: center;
-  align-items: center;
-  background-color: ${(({theme}) => theme.colors.background_dark)};
+  background-color: ${(({theme}) => theme.colors.background)};
  `;
 
- export const Title = styled.Text`
-  font-family: ${({theme}) => theme.fonts.regular};
-  font-size: 24px;
-  color: ${(({theme}) => theme.colors.title)};
- `;
+export const Header = styled.View`
+  width: 100%;
+  height: 278px;
+  background-color: ${(({theme}) => theme.colors.primary)};
+`
