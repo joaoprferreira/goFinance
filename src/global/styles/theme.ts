e8203eccf6325868d4eacc987e1532aa740a4f86
João Pedro <joaof082@gmail.com>
2022-05-29 21:56:03 -0300

    feat: building theme with colors and fonts
    Signed-off-by: João Pedro <joaof082@gmail.com>

--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -1,27 +1,27 @@
 export default {
   colors: {
-    primary: '#5636D3',
+    primary: "#5636D3",
 
-    secondary: '#FF872C',
-    secondary_light: 'rgba(255, 135, 44,0.3)',
+    secondary: "#FF872C",
+    secondary_light: "rgba(255, 135, 44,0.3)",
 
-    success: '#12A454',
-    success_light: 'rgba(18, 164, 84,0.3)',
+    success: "#12A454",
+    success_light: "rgba(18, 164, 84,0.3)",
 
-    attention: '#E83F5B',
-    attention_ligh: 'rgba(232, 63, 91, 0.3)',
+    attention: "#E83F5B",
+    attention_light: "rgba(232, 63, 91, 0.3)",
 
-    shape: '#FFFFFF',
+    shape: "#FFFFFF",
     title: "#363F5F",
-    text: '#969CB3',
-    text_dark: '#000000',
-    background: '#F0F2F5',
-    background_dark: 'rgba(86, 54, 211, 0.5)'
+    text: "#969CB3",
+    text_dark: "#000000",
+    background: "#F0F2F5",
+    background_dark: "rgba(86, 54, 211, 0.5)",
   },
 
   fonts: {
-    regular: 'Poppins_400Regular',
-    medium: 'Poppins_500Medium',
-    bold: 'Poppins_700Bold',
-  }
-}
+    regular: "Poppins_400Regular",
+    medium: "Poppins_500Medium",
+    bold: "Poppins_700Bold",
+  },
+};
