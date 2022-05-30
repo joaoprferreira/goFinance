7045d6295d1d5a37618cfa929f6b0b8aff702058
João Pedro <joaof082@gmail.com>
2022-05-29 21:55:03 -0300

    feat: building the transaction type component and styling
    Signed-off-by: João Pedro <joaof082@gmail.com>

---
+++ src/components/Form/TransactionTypeButton/index.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { TouchableOpacityProps } from "react-native";
+import {
+  Container,
+  Icon,
+  Title,
+} from "./styles";
+
+const icons = {
+  up: 'arrow-up-circle',
+  down: 'arrow-down-circle',
+}
+
+interface Props extends TouchableOpacityProps {
+  isActive: boolean;
+  type: 'up' | 'down';
+  title: string;
+}
+
+export function TransactionTypeButton({
+  isActive,
+  type,
+  title,
+  ...rest
+}: Props) {
+  return (
+    <Container
+      isActive={isActive}
+      type={type}
+      {...rest}>
+      <Icon
+        name={icons[type]}
+        type={type}
+      />
+      <Title>
+        {title}
+      </Title>
+    </Container>
+  )
+}
